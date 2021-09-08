760ef79a0a0a57fed595e621e055d792ff95f041
dschmidt-hgu <dominik.schmidt@hs-gm.de>
2021-09-08 11:46:40 +0200

    initial test page

---
+++ _site.R
@@ -0,0 +1,11 @@
+## packages loaded for all pages 
+suppressPackageStartupMessages({
+  library(data.table)
+  library(ggplot2)
+})
+
+## variables 
+
+
+## knitr options I want set as default for all ('global') code chunks
+knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
